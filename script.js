b5faad52909d4ca45b867b56146cbf4411c77b31
VAYR_hugo <77483277+sartek430@users.noreply.github.com>
2022-05-02 17:41:09 +0200

    formation-js-1

--- script.js
+++ script.js
@@ -23,3 +23,33 @@ var typed = new Typed(".typed", {
   ],
   typeSpeed: 20,
 });
+
+//compteur live
+let compteur = 0;
+
+$(window).scroll(function () {
+  const top = $(".counter").offset().top - window.innerHeight;
+
+  if (compteur == 0 && $(window).scrollTop() > top) {
+    $(".counter-value").each(function () {
+      let $this = $(this),
+        countTo = $this.attr("data-count");
+      $({
+        countNum: $this.text(),
+      }).animate({
+        countNum: countTo,
+      }),
+        {
+          duration: 10000,
+          easing: "swing",
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          },
+        };
+    });
+    compteur = 1;
+  }
+});
